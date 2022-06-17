6e61b7cf15f57725f8abb8a53dba78685e81060e
saff-it <saff_it@ymail.com>
2022-06-17 17:15:35 +0200

    exercise 1 completed

--- script.js
+++ script.js
@@ -9,21 +9,32 @@ const emailList = [
 const userEmail = prompt('Inserisci la tua email');
 let emailMatch = false;
 
+
 for ( let index = 0; index < emailList.length ; index++ ) {
    
     console.log(emailList[index]);
 
     if ( userEmail == emailList[index] ) {
-        console.log(emailList[index]);
         emailMatch = true;
-        alert("Accesso eseguito.");
-
+        
     } 
-
+  
 }
 
-console.log(emailMatch);
-alert("Email sconosciuta, accesso negato.");
+
+if (emailMatch == true) {
+    alert("Accesso eseguito.");
+
+} else if (emailMatch == false) {
+
+    alert("Email sconosciuta, accesso negato.");
+}
+
+
+
+
+
+
 
 
    
