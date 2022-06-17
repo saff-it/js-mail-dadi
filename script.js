9516d0765722a86fd0ced414a238c92a3709ac37
saff-it <saff_it@ymail.com>
2022-06-17 16:00:57 +0200

    exercise 1 testing, if not working

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
+const emailList = [
+    'topolino@walt-disney.com',
+    'paperino@walt-disney.com',
+    'pluto@walt-disney.com',
+    'pippo@walt-disney.com',    
+];
 
+const userEmail = prompt('Inserisci la tua email');
+
+for ( index = 0; index < emailList.length ; index++ );{
+
+
+    if ( userEmail == emailList[index] ) {
+        alert("Hai effettuato l'accesso!");
+
+    } else {
+        alert( "Email sconosciuta, accesso negato." );
+
+    }
+
+    console.log(emailList[index]);
+}
